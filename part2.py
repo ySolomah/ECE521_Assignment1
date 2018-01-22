f3589a6a0698c1f46a15252259c48abdab184920
ySolomah <yassir.solomah@gmail.com>
2018-01-21 23:02:13 -0500

    part 3

--- part2.py
+++ part2.py
@@ -20,25 +20,24 @@ sess = tf.Session()
 guesses = []
 
 def getResponsibilities(data_matrix, target_matrix, data_valid, target_valid, pairwise_matrix, k):
-	print("Top : " + str(k))
-	temp_array = tf.transpose(tf.reciprocal(result), perm=[2, 3, 0, 1])
+	#print("Top : " + str(k))
+	temp_array = tf.transpose(tf.reciprocal(pairwise_matrix), perm=[2, 3, 0, 1])
 	temp_array_shape = (sess.run(temp_array)).shape
-	print("Temp array shape: " + str(temp_array_shape))
+	#print("Temp array shape: " + str(temp_array_shape))
 	responsibilities_array = [0] * temp_array_shape[3]
-	print("Resp array shape: " + (str(len(responsibilities_array))))
+	#print("Resp array shape: " + (str(len(responsibilities_array))))
 	values, indices = tf.nn.top_k(temp_array, k)
 	values = sess.run(tf.reshape(values, [k]))
 	indices = sess.run(tf.reshape(indices, [k]))
-	print("Shape of values: " + str(values.shape))
-	print("Shape of indices: " + str(indices.shape))
-	print("Validation data: " + str(data_valid) + " valid target: " + str(target_valid))
+	#print("Shape of values: " + str(values.shape))
+	#print("Shape of indices: " + str(indices.shape))
+	#print("Validation data: " + str(data_valid) + " valid target: " + str(target_valid))
 	for i in range(len(indices)):
-		print("Index: " + str(indices[i]) + " (casted as int): " + str(int(indices[i])) + " with dataVal: " + str(data_matrix[int(indices[i])]) + " with target: " + str(target_matrix[(indices[i])]))
+		#print("Index: " + str(indices[i]) + " (casted as int): " + str(int(indices[i])) + " with dataVal: " + str(data_matrix[int(indices[i])]) + " with target: " + str(target_matrix[(indices[i])]))
 		responsibilities_array[int(indices[i])] = 1/k
 	return(responsibilities_array)
 
-
-
+'''
 testDataTensor = tf.convert_to_tensor(testData[0].astype(np.float32))
 result = part1.pairwise_square_euclid_distance(testDataTensor, trainDataTensor)
 
@@ -78,4 +77,38 @@ for i in range(len(indices)):
 print("Guesses:")
 print(guesses)
 print(testTarget[0])
+'''
+
+def getTotalResponsibilities(train_data, valid_data, test_data, train_tar, valid_tar, test_tar, k):
+	resp_total = {}
+	if(len(train_data.shape) == 3):
+		train_data = train_data.reshape((train_data.shape[0], train_data.shape[1] * train_data.shape[2]))	
+		valid_data = valid_data.reshape((valid_data.shape[0], valid_data.shape[1] * valid_data.shape[2]))
+		test_data = test_data.reshape((test_data.shape[0], test_data.shape[1] * test_data.shape[2]))
+	train_data_tensor = tf.convert_to_tensor(train_data.astype(np.float32))
+	for i in range(len(test_data)):
+		print("Processing test_data: " + str(i))
+		test_data_tensor = tf.convert_to_tensor(test_data[i].astype(np.float32))
+		result = part1.pairwise_square_euclid_distance(test_data_tensor, train_data_tensor)
+		resp_local = getResponsibilities(train_data, train_tar, test_data[i], test_tar[i], result, k)
+		resp_total[i] = resp_local
+	return(resp_total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ part3.py
@@ -0,0 +1,64 @@
+import argparse
+import numpy as np
+import tensorflow as tf
+import part1
+import part2
+import sys
+import operator
+
+sess = tf.Session()
+
+
+def data_segmentation(data_path, target_path, task):
+	# task = 0 >> select the name ID targets for face recognition task
+	# task = 1 >> select the gender ID targets for gender recognition task
+	data = np.load(data_path)/255
+	data = np.reshape(data, [-1, 32*32])
+	target = np.load(target_path)
+	np.random.seed(45689)
+	rnd_idx = np.arange(np.shape(data)[0])
+	np.random.shuffle(rnd_idx)
+	trBatch = int(0.8*len(rnd_idx))
+	validBatch = int(0.1*len(rnd_idx))
+	trainData, validData, testData = data[rnd_idx[1:trBatch],:], \
+	                                 data[rnd_idx[trBatch+1:trBatch + validBatch],:],\
+	                                 data[rnd_idx[trBatch + validBatch+1:-1],:]
+	trainTarget, validTarget, testTarget = target[rnd_idx[1:trBatch], task], \
+	                            target[rnd_idx[trBatch+1:trBatch + validBatch], task],\
+	                            target[rnd_idx[trBatch + validBatch + 1:-1], task]
+	return trainData, validData, testData, trainTarget, validTarget, testTarget
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_data', required=True)
+parser.add_argument('--input_target', required=True)
+args = parser.parse_args()
+
+train_data, valid_data, test_data, train_tar, valid_tar, test_tar = data_segmentation(args.input_data, args.input_target, 0)
+
+test_data = test_data[0:5]
+test_tar = test_tar[0:5]
+
+validation_mse = {}
+predict_labels = {}
+
+for k in [1, 3, 5, 50]:
+	print("K NOW: " + str(k))
+	validation_mse[k] = 0
+	testResp = part2.getTotalResponsibilities(train_data, valid_data, test_data, train_tar, valid_tar, test_tar, k)
+	for key, resp in testResp.items():
+		for i in range(6):
+			predict_labels[i] = 0
+		guess = 0
+		for j, weight in enumerate(resp):
+			guess += weight * train_tar[j]
+			if(weight > 0):
+				predict_labels[train_tar[j]] += 1
+		print("Predictions for image " + str(key) + " are: ")
+		max_entry = max(predict_labels.items(), key=operator.itemgetter(1))
+		print(str(max_entry[0]) + " with total count: " + str(max_entry[1]) + " remainder: " + str(k-max_entry[1]))
+		print("Correct answer: " + str(test_tar[key]) + "\n")
+
+
+		#print(predict_labels)
+		#print("MSE with k: " + str(k) + " for image: " + str(key) + " is: " + str(((guess - test_tar[key])**2)))
+		validation_mse[k] += (1/len(test_data)) * (guess - test_tar[key])**2
